4af9b5be08dc30b15e8166a2f38490192a132f00
Cef Ramirez <cef.ramirez@gmail.com>
2016-07-14 01:04:14 +0000

    adding controller mqtt

--- scripts/start_lora_server.sh
+++ scripts/start_lora_server.sh
@@ -7,4 +7,4 @@ kill -9 `ps ax | grep loraserver -m 1 | awk '{print $1}'`
 . /etc/lora/networkserver.env
 
 # launch the app
-loraserver --net-id $NET_ID --band $BAND --postgres-dsn $POSTGRES_DSN --gw-mqtt-server $GW_MQTT_SERVER --app-mqtt-server $APP_MQTT_SERVER  --db-automigrate $DB_AUTOMIGRATE --redis-url $REDIS_URL >> /var/log/lora-network-server/lora-network-server.log 2>&1 &
+loraserver --net-id $NET_ID --band $BAND --postgres-dsn $POSTGRES_DSN --controller-mqtt-server $CONTROLLER_MQTT_SERVER --gw-mqtt-server $GW_MQTT_SERVER --app-mqtt-server $APP_MQTT_SERVER  --db-automigrate $DB_AUTOMIGRATE --redis-url $REDIS_URL >> /var/log/lora-network-server/lora-network-server.log 2>&1 &
