3c6bc2999cb5b79948136aaee45a4630939fb956
Cef Ramirez <ccceeefff@users.noreply.github.com>
2016-07-19 16:49:16 -0700

    Update start_lora_server.sh

--- scripts/start_lora_server.sh
+++ scripts/start_lora_server.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -x
+
 # terminate all previous running instances
 kill -9 `ps ax | grep loraserver -m 1 | awk '{print $1}'`
 
@@ -7,4 +9,4 @@ kill -9 `ps ax | grep loraserver -m 1 | awk '{print $1}'`
 . /etc/lora/networkserver.env
 
 # launch the app
-loraserver --net-id $NET_ID --band $BAND --postgres-dsn $POSTGRES_DSN --controller-mqtt-server $CONTROLLER_MQTT_SERVER --gw-mqtt-server $GW_MQTT_SERVER --app-mqtt-server $APP_MQTT_SERVER  --db-automigrate $DB_AUTOMIGRATE --redis-url $REDIS_URL >> /var/log/lora-network-server/lora-network-server.log 2>&1 &
+loraserver --net-id "$NET_ID" --band "$BAND" --redis-url "$REDIS_URL" --postgres-dsn "$POSTGRES_DSN" --controller-mqtt-server "$CONTROLLER_MQTT_SERVER" --gw-mqtt-server "$GW_MQTT_SERVER" --app-mqtt-server "$APP_MQTT_SERVER"  --db-automigrate "$DB_AUTOMIGRATE" >> /var/log/lora-network-server/lora-network-server.log 2>&1 &
